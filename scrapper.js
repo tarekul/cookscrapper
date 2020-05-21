4c28922f1cd973ee7157dab5602f8fdf8636dfb5
Pursuit <pursuit@Pursuits-MacBook-Air.local>
2020-05-21 08:10:29 -0400

    added new args to launch

--- scrapper.js
+++ scrapper.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 async function scrapeRecipe(url) {
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox"]
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
   });
   const page = await browser.newPage();
   await page.goto(url);
