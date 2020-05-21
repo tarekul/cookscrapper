649e609b758d1ade753c4a29bc1fc05175d7affa
Pursuit <pursuit@Pursuits-MacBook-Air.local>
2020-05-21 08:47:39 -0400

    c

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
