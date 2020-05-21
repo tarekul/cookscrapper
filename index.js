fad95dde2b25b15011057ca12249e82331769004
Pursuit <pursuit@Pursuits-MacBook-Air.local>
2020-05-21 11:39:28 -0400

    trial and error

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Methods", "GET");
   next();
 });
 
