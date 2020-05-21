4ca760ea2099622a8434dd7283ca3b37e9a92331
Pursuit <pursuit@Pursuits-MacBook-Air.local>
2020-05-21 19:32:41 -0400

    disabled security on puppeteer

--- scrapper.js
+++ scrapper.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 async function scrapeRecipe(url) {
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-web-security"]
   });
   const page = await browser.newPage();
   await page.goto(url);
