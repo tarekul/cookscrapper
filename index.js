569a9c6482e23edf7cf1877be563fa9d3fa27813
Pursuit <pursuit@Pursuits-MacBook-Air.local>
2020-05-21 20:44:39 -0400

    last attempt

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", async (req, res) => {
   const { web_url } = req.body;
   const result = await cheerioScrape(web_url);
